ffca5a404fe6ea0a231f24554c0a8d03260fdc3f
Joel Stevick <joelstevick@outlook.com>
2024-07-28 19:01:33 -0400

    add second test to verify that we can parse multiple tags

--- html-parser/test_code.py
+++ html-parser/test_code.py
@@ -2,8 +2,8 @@ import unittest
 from code import html_parse, State
 
 class TestHtmlParser(unittest.TestCase):
-    # can parse <hello>World</hello>
-    def test_parse_simple_tag(self):
+    # can parse a single tag
+    def test_parse_single_tag(self):
         html = '<hello>World</hello>'
                
         context = html_parse(html, {
@@ -15,6 +15,23 @@ class TestHtmlParser(unittest.TestCase):
         self.assertEqual(len(context["tags"]), 1)
         
         self.assertEqual(context["tags"][0]["name"], "hello")
-    
+        
+    # can parse multiple tags
+    def test_parse_multiple_tags(self):
+        html = '<hello>World</hello><foo>bar</foo>'
+               
+        context = html_parse(html, {
+            "state": State.NULL,
+            "tags": [],
+            "tag_name": ''
+        })
+        
+        self.assertEqual(len(context["tags"]), 2)
+        
+        self.assertEqual(context["tags"][0]["name"], "hello")
+        
+        self.assertEqual(context["tags"][1]["name"], "foo")
+
+
 if __name__ == '__main__':
     unittest.main()
