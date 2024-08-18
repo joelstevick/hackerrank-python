d13346e65a90f8c8167a17f4bd21b09ca7b7c9dc
Joel Stevick <joelstevick@outlook.com>
2024-08-18 14:00:09 -0400

    refactor recursive function into while loop

--- minion-game/vowels.py
+++ minion-game/vowels.py
@@ -1,33 +1,36 @@
 vowels = {
-           'A': True,
-           'E': True,
-           'I': True,
-           'O': True,
-           'U': True
+    'A': True,
+    'E': True,
+    'I': True,
+    'O': True,
+    'U': True
 }
 stuart = 0
 kevin = 0
 
 COUNT = "@*c*@"
 
-def is_vowel(string): 
+
+def is_vowel(string):
     return vowels.get(string[0]) == True
-    
+
+
 root = {}
+
 
 def add_node(char, parent):
     node = parent.get(char)
     if node == None:
         node = {
-            COUNT : 1
+            COUNT: 1
         }
         parent[char] = node
     else:
         node[COUNT] += 1
-    
+
     return node
 
-        
+
 def build_tree(string):
     global root
     global stuart
@@ -36,32 +39,43 @@ def build_tree(string):
     for i in range(len(string)):
         parent = None
         for j in range(len(string) - i):
-            char = string[i+j]
-            
+            char = string[i + j]
+
             if not parent:
                 parent = add_node(char, root)
             else:
                 parent = add_node(char, parent)
 
-def traverse(char, parent):
-    node = parent.get(char)
-    count = node[COUNT]
 
-    for _, char in enumerate(node.keys()):
-        if char == COUNT:
-            continue
-        
-        count += traverse(char, node)
+def traverse(init_char, parent):
+    stack = [{"char": init_char, "parent": parent}]
+    count = 0
+
+    while stack:
+        current = stack.pop()
+
+        char = current["char"]
+        parent = current["parent"]
+
+        node = parent.get(char)
+        count += node[COUNT]
+
+        for _, char2 in enumerate(node.keys()):
+            if char2 == COUNT:
+                continue
+
+            stack.append({"char": char2, "parent": node})
 
     return count
 
+
 def minion_game(string):
     build_tree(string)
-    
+
     global stuart
     global kevin
     global root
-    
+
     for _, key in enumerate(root.keys()):
         if is_vowel(key):
             kevin += traverse(key, root)
@@ -74,8 +88,8 @@ def minion_game(string):
         print(f"Kevin {kevin}")
     else:
         print("Draw")
-        
-        
+
+
 if __name__ == '__main__':
     s = input()
     minion_game(s)
